daa70e8182e283d741ca7b837385ec1024be55a2
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-05-25 17:37:32 +0530

    added files

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
 
     int main() {
-    int a , b;
-    printf("enter a ");
-    scanf("%d", &a);
-    printf("enter b ");
-    scanf("%d", &b);
-    int sum = a + b;
-    printf("the sum is : %d", sum);
+    printf("hello world");
     return 0;
     }
     
