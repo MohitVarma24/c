ca5af46a1bab4b3964ca18fdb4f8570c441e2546
MohitVarma24 <mohitvarma24aug2006@gmail.com>
2024-09-02 13:53:13 +0530

    added files

---
+++ apnacollege/chapter1/squarea.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int side;
+    printf("enter the side ");
+    scanf("%d", &side);
+    printf("the area of the square is : %d", side * side);
+    return 0;
+}
---
+++ apnacollege/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+    int a, b;
+
+    printf("\nEnter the value : ");
+    scanf("%d", &a);
+    printf("\nEnter the value : ");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("\nThe sum of a & b is : %d", sum);
+
+return 0;
+}
